e38a90f49561c652e0f1692e08316721db459b6f
Steven Masley <stevenmasley@gmail.com>
2025-12-22 10:50:26 -0500

    add compiled

--- scripts/compiledregex/main.go
+++ scripts/compiledregex/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/Emyrk/chronicle/combatlog/parser/regexs"
 	"github.com/KromDaniel/regengo"
@@ -12,21 +14,22 @@ import (
 // Optimizing regex performance by compiling frequently used regex patterns into Go code.
 func main() {
 	var (
-		outFile = flag.String("output", "compiled.go", "Output file for the compiled regex Go code.")
-		pkg     = flag.String("package", "compiled", "Package name for the compiled regex Go code.")
+		outDir = flag.String("output", ".", "Output directory for the compiled regex Go code.")
+		pkg    = flag.String("package", "compiled", "Package name for the compiled regex Go code.")
 	)
 	flag.Parse()
 
 	compile := map[string]*regexp.Regexp{
 		"ReSpellCastAttempt":        regexs.ReSpellCastAttempt,
 		"ReBugDamageSpellHitOrCrit": regexs.ReBugDamageSpellHitOrCrit,
+		"ReCreates":                 regexs.ReCreates,
 	}
 
 	for name, pattern := range compile {
 		err := regengo.Compile(regengo.Options{
 			Pattern:    pattern.String(),
 			Name:       name,
-			OutputFile: *outFile,
+			OutputFile: filepath.Join(*outDir, strings.ToLower(name)+".go"),
 			Package:    *pkg,
 		})
 		if err != nil {

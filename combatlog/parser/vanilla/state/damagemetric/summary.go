fa26b52e25ce0cbbacb90caaec607ceae8b6cab2
Steven Masley <stevenmasley@gmail.com>
2025-12-25 18:06:03 -0500

    identify some bosses

--- combatlog/parser/vanilla/state/damagemetric/summary.go
+++ combatlog/parser/vanilla/state/damagemetric/summary.go
@@ -1,38 +1,37 @@
 package damagemetric
 
 import (
-  "time"
+	"time"
 
-  "github.com/Emyrk/chronicle/combatlog/parser/guid"
+	"github.com/Emyrk/chronicle/combatlog/parser/guid"
 )
 
 type Summary struct {
-  Start time.Time
-  End   time.Time
+	Start time.Time
+	End   time.Time
 
-  // TotalDealt damage dealt by each unit from each source (like a spell)
-  TotalDealt map[guid.GUID]map[string]int32
-  //Taken map[guid.GUID]
+	// TotalDealt damage dealt by each unit from each source (like a spell)
+	TotalDealt map[guid.GUID]map[string]int32
 }
 
 func (d *Damage) Summary(start time.Time, end time.Time) (Summary, error) {
-  summary := Summary{
-    Start:      start,
-    End:        end,
-    TotalDealt: make(map[guid.GUID]map[string]int32),
-  }
+	summary := Summary{
+		Start:      start,
+		End:        end,
+		TotalDealt: make(map[guid.GUID]map[string]int32),
+	}
 
-  err := d.Range(start, end, func(e Event) {
-    if e.Source != nil {
-      if summary.TotalDealt[*e.Source] == nil {
-        summary.TotalDealt[*e.Source] = make(map[string]int32)
-      }
-      summary.TotalDealt[*e.Source][e.From] += e.Amount
-    }
-  })
-  if err != nil {
-    return summary, err
-  }
+	err := d.Range(start, end, func(e Event) {
+		if e.Source != nil {
+			if summary.TotalDealt[*e.Source] == nil {
+				summary.TotalDealt[*e.Source] = make(map[string]int32)
+			}
+			summary.TotalDealt[*e.Source][e.From] += e.Amount
+		}
+	})
+	if err != nil {
+		return summary, err
+	}
 
-  return summary, nil
+	return summary, nil
 }

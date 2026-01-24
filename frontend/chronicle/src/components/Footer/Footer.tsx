d5e38b4028fb97031cba9a4b8d6ebfa57c08a730
Steven Masley <stevenmasley@gmail.com>
2026-01-24 13:06:50 -0600

    add build info

--- frontend/chronicle/src/components/Footer/Footer.tsx
+++ frontend/chronicle/src/components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
 export function Footer() {
+  const gitTag = document
+    .querySelector("meta[property=GitTag]")
+    ?.getAttribute("content");
+
+  const gitCommit = document
+    .querySelector("meta[property=GitCommit]")
+    ?.getAttribute("content");
+
+  const buildTime = document
+    .querySelector("meta[property=BuildTime]")
+    ?.getAttribute("content");
+
   return (
     <footer className="border-t p-4 text-center text-sm text-muted-foreground">
-      <p>© {new Date().getFullYear()} Chronicle. All rights reserved.</p>
+      <div className="flex flex-col items-center gap-1">
+        <p>© {new Date().getFullYear()} Chronicle. All rights reserved.</p>
+        <p className="text-xs">
+          {gitTag} ({gitCommit}) • Built {buildTime}
+        </p>
+      </div>
     </footer>
   );
 }

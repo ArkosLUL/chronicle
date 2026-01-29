b421aaa0142c00059e175c00af124c59bdb37050
Steven Masley <stevenmasley@gmail.com>
2026-01-29 09:47:54 -0600

    Add unit name

--- combatlog/parser/unitname/unitname.go
+++ combatlog/parser/unitname/unitname.go
@@ -1,5 +1,7 @@
 package unitname
 
+// ByEntry is a static list of some unit names by their entry ID. Helps when
+// identifying units in the combat log if their name is not given from the addon.
 func ByEntry(entry uint32) string {
 	switch entry {
 	case 89:

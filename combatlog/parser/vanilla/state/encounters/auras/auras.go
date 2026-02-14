537b4596239069d258621b2009199c3ad5815035
Steven Masley <stevenmasley@gmail.com>
2026-02-14 05:59:47 -0600

    docs: add agent-skills README catalog

--- combatlog/parser/vanilla/state/encounters/auras/auras.go
+++ combatlog/parser/vanilla/state/encounters/auras/auras.go
@@ -1,1 +1,19 @@
 package auras
+
+import (
+	"github.com/Emyrk/chronicle/combatlog/parser/guid"
+	"github.com/Emyrk/chronicle/combatlog/parser/vanilla/messages"
+)
+
+type Tracking struct {
+	units map[guid.GUID]map[string]int
+}
+
+func New() *Tracking {
+	return &Tracking{}
+}
+
+func (t *Tracking) Process(m messages.Message) error {
+
+	return nil
+}

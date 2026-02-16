72a2bf6b883a1082b2c1bc9e67ec2b9b052e2be3
Steven Masley <stevenmasley@gmail.com>
2026-02-16 10:22:59 -0600

    cancel migration

--- database/migrations/000019_expand_spell_templates.down.sql
+++ database/migrations/000019_expand_spell_templates.down.sql
@@ -1,17 +1,4 @@
 BEGIN;
 
-ALTER TABLE spell_templates 
-  DROP COLUMN IF EXISTS subtext,
-  DROP COLUMN IF EXISTS aura_description,
-  DROP COLUMN IF EXISTS icon_id,
-  DROP COLUMN IF EXISTS school_mask,
-  DROP COLUMN IF EXISTS power_type,
-  DROP COLUMN IF EXISTS mana_cost,
-  DROP COLUMN IF EXISTS mana_cost_pct,
-  DROP COLUMN IF EXISTS cast_time_index,
-  DROP COLUMN IF EXISTS recovery_time,
-  DROP COLUMN IF EXISTS range_index,
-  DROP COLUMN IF EXISTS attributes,
-  DROP COLUMN IF EXISTS targets;
 
 COMMIT;
--- database/migrations/000019_expand_spell_templates.up.sql
+++ database/migrations/000019_expand_spell_templates.up.sql
@@ -1,29 +1,30 @@
 BEGIN;
 
+-- Cancelled migration
 -- Expand spell_templates with additional fields for tooltip display and filtering
-ALTER TABLE spell_templates 
-  ADD COLUMN IF NOT EXISTS subtext TEXT DEFAULT '',
-  ADD COLUMN IF NOT EXISTS aura_description TEXT DEFAULT '',
-  ADD COLUMN IF NOT EXISTS icon_id INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS school_mask INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS power_type INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS mana_cost INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS mana_cost_pct INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS cast_time_index INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS recovery_time INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS range_index INT DEFAULT 0,
-  ADD COLUMN IF NOT EXISTS attributes BIGINT[] DEFAULT '{}',
-  ADD COLUMN IF NOT EXISTS targets INT DEFAULT 0;
-
-COMMENT ON COLUMN spell_templates.subtext IS 'Rank text like "Rank 1", "Passive", etc.';
-COMMENT ON COLUMN spell_templates.aura_description IS 'Description shown when spell is an aura/buff';
-COMMENT ON COLUMN spell_templates.icon_id IS 'SpellIconID for icon lookup';
-COMMENT ON COLUMN spell_templates.school_mask IS 'Bitmask: 1=phys, 2=holy, 4=fire, 8=nature, 16=frost, 32=shadow, 64=arcane';
-COMMENT ON COLUMN spell_templates.power_type IS '0=mana, 1=rage, 2=focus, 3=energy';
-COMMENT ON COLUMN spell_templates.cast_time_index IS 'Index into SpellCastTimes.dbc';
-COMMENT ON COLUMN spell_templates.recovery_time IS 'Cooldown in milliseconds';
-COMMENT ON COLUMN spell_templates.range_index IS 'Index into SpellRange.dbc';
-COMMENT ON COLUMN spell_templates.attributes IS '9-element array of uint32 attribute bitmasks';
-COMMENT ON COLUMN spell_templates.targets IS 'Target flags bitmask';
+-- ALTER TABLE spell_templates
+--   ADD COLUMN IF NOT EXISTS subtext TEXT DEFAULT '',
+--   ADD COLUMN IF NOT EXISTS aura_description TEXT DEFAULT '',
+--   ADD COLUMN IF NOT EXISTS icon_id INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS school_mask INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS power_type INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS mana_cost INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS mana_cost_pct INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS cast_time_index INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS recovery_time INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS range_index INT DEFAULT 0,
+--   ADD COLUMN IF NOT EXISTS attributes BIGINT[] DEFAULT '{}',
+--   ADD COLUMN IF NOT EXISTS targets INT DEFAULT 0;
+--
+-- COMMENT ON COLUMN spell_templates.subtext IS 'Rank text like "Rank 1", "Passive", etc.';
+-- COMMENT ON COLUMN spell_templates.aura_description IS 'Description shown when spell is an aura/buff';
+-- COMMENT ON COLUMN spell_templates.icon_id IS 'SpellIconID for icon lookup';
+-- COMMENT ON COLUMN spell_templates.school_mask IS 'Bitmask: 1=phys, 2=holy, 4=fire, 8=nature, 16=frost, 32=shadow, 64=arcane';
+-- COMMENT ON COLUMN spell_templates.power_type IS '0=mana, 1=rage, 2=focus, 3=energy';
+-- COMMENT ON COLUMN spell_templates.cast_time_index IS 'Index into SpellCastTimes.dbc';
+-- COMMENT ON COLUMN spell_templates.recovery_time IS 'Cooldown in milliseconds';
+-- COMMENT ON COLUMN spell_templates.range_index IS 'Index into SpellRange.dbc';
+-- COMMENT ON COLUMN spell_templates.attributes IS '9-element array of uint32 attribute bitmasks';
+-- COMMENT ON COLUMN spell_templates.targets IS 'Target flags bitmask';
 
 COMMIT;

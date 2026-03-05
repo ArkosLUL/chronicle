9e3737c7b38d029b90342b89be7c33fc2596a68b
Steven Masley <stevenmasley@gmail.com>
2026-03-05 06:49:16 -0600

    remove authz migration

--- database/authz/migrations.go
+++ database/authz/migrations.go
@@ -2,26 +2,24 @@ package authz
 
 import (
 	"context"
-
-	"github.com/Emyrk/chronicle/database/authz/policy"
 )
 
 func RunSchemaMigrations(ctx context.Context, az *Authz) error {
-	rows, err := az.ListAllWoWLogGroupsWithOwner(ctx)
-	if err != nil {
-		return err
-	}
-
-	b := policy.New()
-	chron := b.GlobalChronicle()
-	for _, row := range rows {
-		b.Raid_log(row.WoWLogGroup.ID).
-			Chronicle(chron).Uploader(b.User(row.WoWLogGroup.Owner))
-	}
-	_, err = az.Write(ctx, *b.Txn())
-	if err != nil {
-		return err
-	}
+	//rows, err := az.ListAllWoWLogGroupsWithOwner(ctx)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//b := policy.New()
+	//chron := b.GlobalChronicle()
+	//for _, row := range rows {
+	//	b.Raid_log(row.WoWLogGroup.ID).
+	//		Chronicle(chron).Uploader(b.User(row.WoWLogGroup.Owner))
+	//}
+	//_, err = az.Write(ctx, *b.Txn())
+	//if err != nil {
+	//	return err
+	//}
 
 	return nil
 }

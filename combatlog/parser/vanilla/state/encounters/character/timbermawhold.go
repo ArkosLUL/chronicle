4a52da496f30dd932a6bfc9b275f19e6e46529b8
Steven Masley <stevenmasley@gmail.com>
2026-03-28 10:15:26 -0500

    timbermaw changes

--- combatlog/parser/vanilla/state/encounters/character/timbermawhold.go
+++ combatlog/parser/vanilla/state/encounters/character/timbermawhold.go
@@ -4,7 +4,7 @@ import "github.com/Emyrk/chronicle/combatlog/parser/guid"
 
 func NewKarrsh(id guid.GUID, all *Characters) (Character, bool) {
 	if entry, ok := id.GetEntry(); !ok || entry != 62934 {
-
+		return nil, false
 	}
 	return NewPermanentDeath(NewCommonCharacter(id, all)), true
 }

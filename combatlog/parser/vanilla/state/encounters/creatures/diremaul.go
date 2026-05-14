53a581a044bd1c512025909187b8f1cb270feb92
Steven Masley <stevenmasley@gmail.com>
2026-05-14 16:15:40 -0500

    fix king

--- combatlog/parser/vanilla/state/encounters/creatures/diremaul.go
+++ combatlog/parser/vanilla/state/encounters/creatures/diremaul.go
@@ -14,8 +14,7 @@ func NewImmolthar(id guid.GUID, all *characters.Characters) (characters.Characte
 }
 
 func NewKingGordok(id guid.GUID, all *characters.Characters) (characters.Character, bool) {
-	return characters.NewAdsGoWithBoss(11496,
-		11501, // King Gordok
+	return characters.NewAdsGoWithBoss(11501, // King Gordok
 		14324, // Observer
 	)(id, all)
 }

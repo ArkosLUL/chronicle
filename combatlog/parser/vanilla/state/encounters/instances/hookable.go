c82ea865fa5da8c774bc3f503907029783c0e891
Steven Masley <stevenmasley@gmail.com>
2026-05-27 09:07:16 -0500

    fix panic deref

--- combatlog/parser/vanilla/state/encounters/instances/hookable.go
+++ combatlog/parser/vanilla/state/encounters/instances/hookable.go
@@ -725,11 +725,13 @@ func (e *encounterName) applyState(ch CharacterFight, id Identity, f Fight) {
 
 	if id.EncounterNameFn != nil {
 		res := id.EncounterNameFn(f)
-		if res != nil && res.EncounterName != "" || len(res.Bosses) > 0 {
-			e.encounterType = types.EncounterTypeBOSS
-		}
-		for _, r := range res.Bosses {
-			e.bossDeadState[r] = false
+		if res != nil {
+			if res.EncounterName != "" || len(res.Bosses) > 0 {
+				e.encounterType = types.EncounterTypeBOSS
+			}
+			for _, r := range res.Bosses {
+				e.bossDeadState[r] = false
+			}
 		}
 	}
 }

12b183c8d77d058958a011825bbf7a9472a7223a
Steven Masley <stevenmasley@gmail.com>
2026-07-27 14:09:24 -0500

    change

--- scripts/upload-dbc/legacy.sh
+++ scripts/upload-dbc/legacy.sh
@@ -16,6 +16,7 @@ TARGETS=(
 #  "azerothcore|http://localhost:4000|00000000-0000-0000-0000-000000000001"
 #  "turtle|http://localhost:4000|d693ba3c-8bba-421d-8ae9-8eb2a65641cf"
 #  "vanillaplus|http://localhost:4000|e1520e3b-32bb-48b9-b391-d2342a582b4e"
+#  "faebright|http://localhost:4000/|58942539-2d80-44c9-8441-68aca794569c"
 #)
 
 source "$SCRIPT_DIR/run.sh"

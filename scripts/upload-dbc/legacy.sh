016c8aeaa41d32a36e2059c099227f1b1385a0b5
Steven Masley <stevenmasley@gmail.com>
2026-08-05 17:09:47 -0500

    fix octo

--- scripts/upload-dbc/legacy.sh
+++ scripts/upload-dbc/legacy.sh
@@ -9,6 +9,7 @@ TARGETS=(
   "azerothcore|https://legacy.chronicleclassic.com/|e6606f7b-7e9e-4bc2-970b-bde8cd500a6b"
   "kronos|https://legacy.chronicleclassic.com/|0da7611b-a3a1-47d8-82a5-f383c43cd69d"
   "turtle|https://legacy.chronicleclassic.com/|a0404e03-e743-49e5-9876-7d5fa2931159"
+  "octowow|https://legacy.chronicleclassic.com/|2e080ee0-ca21-47d3-a7f9-8c3ba638e1c4"
   "vanillaplus|https://legacy.chronicleclassic.com/|d77b88b5-97e9-4f6b-acc9-c291f546e475"
   "lunatic|https://legacy.chronicleclassic.com/|53f9c96d-2b9a-43d9-8244-ffc6c0bf4ce6"
 )

6dee73e2e2304d9fe5a310d9e418313212008c67
Steven Masley <stevenmasley@gmail.com>
2026-08-11 13:02:58 -0500

    extra script for local

--- scripts/upload-dbc/legacy.sh
+++ scripts/upload-dbc/legacy.sh
@@ -15,11 +15,4 @@ TARGETS=(
   "lunatic|https://legacy.chronicleclassic.com/|53f9c96d-2b9a-43d9-8244-ffc6c0bf4ce6"
 )
 
-#TARGETS=(
-#  "azerothcore|http://localhost:4000|00000000-0000-0000-0000-000000000001"
-#  "turtle|http://localhost:4000|d693ba3c-8bba-421d-8ae9-8eb2a65641cf"
-#  "vanillaplus|http://localhost:4000|e1520e3b-32bb-48b9-b391-d2342a582b4e"
-#  "faebright|http://localhost:4000/|58942539-2d80-44c9-8441-68aca794569c"
-#)
-
 source "$SCRIPT_DIR/run.sh"
